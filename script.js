495a95b9a1cf5b9068c68ee9be6787fb67897e8f
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-03-14 15:53:01 -0400

    Logica de Somar/Resultado/Apagar criada

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 var numeroAtual = "";
+var num1 = null;
+var num2 = null;
+var atuacao = "";
 
+var clickSomar = 0;
 var calcConsole = document.getElementById('calcConsole')
 
 function buttonNumber(num){
     numeroAtual += num
+    atualizarConsole()
+}
+
+function atualizarConsole(){
     calcConsole.innerHTML = numeroAtual
+}
+
+function apagarButton(){
+    numeroAtual = numeroAtual.slice(0, -1)
+    atualizarConsole()   
+}
+
+function somarButton(){
+    atuacao = "somar";
+    if(num1 === null){
+        num1 = parseFloat(numeroAtual)
+        numeroAtual = "";
+        atualizarConsole()
+    }
+}
+
+function criarResultado(){
+    var result;
+    if(atuacao === "somar"){
+        result = num1 + parseFloat(numeroAtual);
+        numeroAtual = result.toString();
+        atualizarConsole();
+        num1 = null;
+    }
 }
